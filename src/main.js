2abff58bf4d0f6deb2dbbbfc3d9341953a0bae6c
bndnt <67003594+bndnt@users.noreply.github.com>
2024-07-06 00:00:38 -0400

    about-me

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Accordion from 'accordion-js';
+new Accordion('.accordion-container', {
+  openOnInit: [0],
+});
+var swiper1 = new Swiper('.swiper1', {
+  slidesPerView: 6,
+  spaceBetween: 0,
+  loop: true,
+  mousewheel: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: '.js-about-slider-arrow',
+    // prevEl: '.swiper-button-prev',
+  },
+});
