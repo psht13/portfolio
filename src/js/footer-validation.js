bb5b3b50959feb5d3f0d0c77101de4d503a9388e
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-07-06 19:59:19 +0300

    upd

--- src/js/footer-validation.js
+++
@@ -1,4 +0,0 @@
-// variables
-const underscores = document.querySelectorAll('.footer-underscore');
-const messageBox = document.querySelector('#message-box');
-const inputs = document.querySelectorAll('.footer-input');
